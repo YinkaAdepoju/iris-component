815575416e162d53d22266840cc6c1db8bce6832
yinka <yinka.social@icloud.com>
2024-05-18 15:11:41 +0300

    Initial Commit

---
+++ iris-component/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iris-component
+//
+//  Created by Yinka Adepoju on 18/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iris-component/iris_componentApp.swift
@@ -0,0 +1,17 @@
+//
+//  iris_componentApp.swift
+//  iris-component
+//
+//  Created by Yinka Adepoju on 18/5/24.
+//
+
+import SwiftUI
+
+@main
+struct iris_componentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
